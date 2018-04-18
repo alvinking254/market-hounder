83529b2668d8a64857979773fefdf6f908ee69f9
zlyxero <mitchyjeffy@gmail.com>
2018-04-18 18:12:16 +0300

    updated production settings

--- market_hounder_project/settings.py
+++ market_hounder_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dgu1rcet(^bk&ftwil0a+r#hi)+nalc5e6%c@#k3w-)rm@e@1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
