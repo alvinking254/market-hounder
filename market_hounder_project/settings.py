a58dd2bb2e801b7cd999eeb54e22c1113f6bf2fc
zlyxero <mitchyjeffy@gmail.com>
2018-04-18 17:59:59 +0300

    updated the STATIC_ROOT setting

--- market_hounder_project/settings.py
+++ market_hounder_project/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
