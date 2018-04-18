9aa653728b011646f7eddbffbc242c3bf9a021fe
zlyxero <mitchyjeffy@gmail.com>
2018-04-18 17:49:02 +0300

    Heroku config files and updates

--- market_hounder_project/settings.py
+++ market_hounder_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dgu1rcet(^bk&ftwil0a+r#hi)+nalc5e6%c@#k3w-)rm@e@1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
